297dbbb8d3954b26782aa383a692877348ad2757
maxthestranger <thestranger@gmail.com>
2022-06-14 11:55:00 +0300

    add class

--- app.rb
+++ app.rb
@@ -1,1 +1,19 @@
-# code below
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = nil
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    true if of_age? || @parent_permission
+  end
+end
